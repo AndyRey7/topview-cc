926859161a95c3bd236ea8a718969551cd922f41
github username <github email address>
2019-09-14 15:49:10 -0400

    alright everything is in order and working great now to start finally

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        bikes: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/products')
+        .then(res => res.json())
+        .then( data => {
+            this.setState({
+                bikes: data
+            }, () => console.log('this is state', this.state.bikes))
+        })
+    }
+  render(){
+      return (
+          <div>
+          Hello
+          </div>
+      )
+  }
 }
 
 export default App;
