84b9d19e9f2d450a32be36c60932c52ab91017b8
github username <github email address>
2019-09-15 16:15:52 -0400

    added validation for selection on bike or helmets

--- src/RentalForm.js
+++ src/RentalForm.js
@@ -3,8 +3,7 @@ import React, {Component} from 'react';
 export default class RentalForm extends Component {
 
     state = {
-        error: false,
-        category: 'Adult Male Bike'
+        error: false
     }
 
     handleErrorClick = () => {
@@ -13,16 +12,32 @@ export default class RentalForm extends Component {
         })
     }
 
-    errorMessage = () => {
-            if (this.state.error === false) throw window.alert("please choose insurance!")
+    checkSelectionErrorMessage = () => {
+        let selectedBike = document.getElementById('bikeOption');
+        let selectedHelmet = document.getElementById('helmetOption');
+        let selectedBikeValue = selectedBike.options[selectedBike.selectedIndex].value;
+        let selectedHelmetValue = selectedHelmet.options[selectedHelmet.selectedIndex].value;
+
+        if (selectedBikeValue && selectedHelmetValue === "selectOption") {
+            alert("please select a bike and helmet please!!");
+        }
     }
+
+    errorMessage = () => {
+            if (this.state.error === false) {
+                this.checkSelectionErrorMessage()
+                window.alert("please choose insurance!")
+            }
+    }
+
     render() {
         return (
-            <form onSubmit={(e) => this.props.handleAdd(e, this.state)}>
+            <form>
                 <h2>Bike Rental Form</h2>
                 <div>
                     <span>Bike</span>
-                    <select onChange={this.handleChange}>
+                    <select id="bikeOption">
+                        <option value="selectOption">--- Please Select ---</option>
                         <option value="Adult Male Bike">Adult Male Bike</option>
                         <option value="Adult Female Bike">Adult Female Bike</option>
                         <option value="Kids Unisex">Kids Unisex Bike</option>
@@ -30,14 +45,15 @@ export default class RentalForm extends Component {
                 </div>
                 <div>
                     <span>Helmet</span>
-                    <select onChange={this.handleChange}>
+                    <select id="helmetOption">
+                        <option value="selectOption">--- Please Select ---</option>
                         <option value="Adult Unisex Helmet">Adult Unisex Helmet</option>
                         <option value="Kids Unisex Helmet">Kids Unisex Helmet</option>
                     </select>
                 </div>
                 <div>
-                <span>Insurance $9.99</span>
-                <input type="checkbox" onClick={this.handleErrorClick} value="Insurance"/>
+                    <span>Insurance $9.99</span>
+                    <input type="checkbox" onClick={this.handleErrorClick} value="Insurance"/>
                 </div>
                 <input type="submit" onClick={() => this.errorMessage()} value="Complete Rental Reservation"/>
             </form>
